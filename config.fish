4840e98c4445faf715fcf20f5eb8f4b2f43433ab
cosven <yinshaowen241@gmail.com>
2017-01-17 15:54:02 +0800

    update fish

--- config.fish
+++ config.fish
@@ -1,2 +1,2 @@
-set -x PATH $PATH /Users/ysw/path /Library/Frameworks/Python.framework/Versions/3.5/bin/python3 /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set -x PATH $PATH /Users/ysw/path /Library/Frameworks/Python.framework/Versions/3.5/bin/ /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
