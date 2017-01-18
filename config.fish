a283eb2676ae67933e8b59be17de79dbf0a60327
cosven <yinshaowen241@gmail.com>
2017-01-18 19:07:25 +0800

    update fish

--- config.fish
+++ config.fish
@@ -1,2 +1,6 @@
 set -x PATH $PATH /Users/ysw/path /Library/Frameworks/Python.framework/Versions/3.5/bin/ /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+abbr -a ec emacsclient -t
+set -x -g LC_ALL en_US.UTF-8
+set -x -g LANG en_US.UTF-8
+
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
