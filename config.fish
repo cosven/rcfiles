1c9217623264b282f093a60e6aa766c53594beeb
cosven <yinshaowen241@gmail.com>
2017-01-19 00:58:30 +0800

    update fish

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-set -x PATH $PATH /Users/ysw/path /Library/Frameworks/Python.framework/Versions/3.5/bin/ /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set -x PATH $PATH ~/path /Library/Frameworks/Python.framework/Versions/3.5/bin/ /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 abbr -a ec emacsclient -t
 set -x -g LC_ALL en_US.UTF-8
 set -x -g LANG en_US.UTF-8
