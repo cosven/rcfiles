8189683a0a311f74a4c1ceed357c96ce9b0af28d
cosven <yinshaowen241@gmail.com>
2017-03-17 14:33:32 +0800

    update purcell emacs config

--- init-local.el
+++ init-local.el
@@ -4,7 +4,7 @@
 (require 'neotree)
 (global-set-key [f2] 'neotree-toggle)
 
-(setq-default custom-enabled-themes '(tango-dark))
+(setq-default custom-enabled-themes '(monoj-dark))
 
 (add-to-list 'default-frame-alist
              '(font . "Monaco-13"))
